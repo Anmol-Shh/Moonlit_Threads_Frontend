fe26b36440fe3902cafa75b9a5d6fbbd7a8530d2
Anmol Sharma <anmolsharma6503@gmail.com>
2025-04-05 23:54:21 +0530

    update

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = import.meta.env.MODE === "development" ? `${import.meta.env.VITE_API_BASE_URL}/api/auth` : "/api/auth";
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/auth` 
 
 axios.defaults.withCredentials = true;
 
